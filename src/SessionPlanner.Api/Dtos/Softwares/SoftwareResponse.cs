ad7771767a248d5697ea8586b2f0eb68e3a4ec1c
Galledonium <44852866+Galledonium@users.noreply.github.com>
2026-02-19 00:04:26 -0500

    SoftwaresController
    -Update GitIgnore
    -Added REST queries for SoftwaresController

--- src/SessionPlanner.Api/Dtos/Softwares/SoftwareResponse.cs
+++ src/SessionPlanner.Api/Dtos/Softwares/SoftwareResponse.cs
@@ -1,3 +1,6 @@
+using SessionPlanner.Api.Dtos.SoftwareVersions;
+
 namespace SessionPlanner.Api.Dtos.Softwares;
 
+//public record SoftwareResponse(int Id, string Name, IEnumerable<SoftwareVersionResponse>);
 public record SoftwareResponse(int Id, string Name);
---
+++ src/SessionPlanner.Infrastructure/Migrations/20260219044351_AddSessions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SessionPlanner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSessions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Softwares",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Softwares",
+                newName: "ID");
+        }
+    }
+}
