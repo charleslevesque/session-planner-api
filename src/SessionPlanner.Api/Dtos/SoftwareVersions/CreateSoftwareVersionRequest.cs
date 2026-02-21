c6d88a78e647f1d8002e36e8fe7586f8665f0f8a
Galledonium <44852866+Galledonium@users.noreply.github.com>
2026-02-20 22:34:38 -0500

    Added softwareVersion controller

--- src/SessionPlanner.Api/Dtos/SoftwareVersions/CreateSoftwareVersionRequest.cs
+++ src/SessionPlanner.Api/Dtos/SoftwareVersions/CreateSoftwareVersionRequest.cs
@@ -1,6 +1,7 @@
 namespace SessionPlanner.Api.Dtos.SoftwareVersions;
 
 public record CreateSoftwareVersionRequest(
+    int SoftwareId,
     string VersionNumber,
     string? InstallationDetails,
     string? Notes
